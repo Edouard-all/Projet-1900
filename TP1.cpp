6fec84cea69fea9cc54fb6c3c66349a149eff4df
Edouard <eefj.allaire@gmail.com>
2026-01-20 15:46:18 -0500

    EA: debut de tp2

--- TP1.cpp
+++ TP1.cpp
@@ -1,48 +1,55 @@
 #define F_CPU 8000000
 #include <avr/io.h>
 #include <util/delay.h>
-const double ms = 1600;
+const double delay = 200;
 void _delay_ms(double ms);
+bool bouton_presse();
 int main() {
-	__builtin_avr_delay_cycles(ms);
 	DDRA |= (1 << PA0) | (1 << PA1);
 	DDRD &= ~(1 << PD2);
 	uint8_t etat = 0;
 	while (true) {
-		if (PIND & (1 << PD2)){
-			_delay_ms(100);
-			if (PIND & (1 << PD2)){
-				switch (etat)
-				{
-				case 0: //rouge
-					PORTA |= (1 << PA0);
-					PORTA &= ~(1 << PA1);
-					etat++;
-					break;
-				case 1: //vert
-					PORTA |= (1 << PA1);
-					PORTA &= ~(1 << PA0);
-					etat++;
-					break;
-				default: //ambre
-					_delay_ms(100);
-					etat = 0;
-					while (true) {
-					PORTA |= (1 << PA0);
-					PORTA &= ~(1 << PA1);
-					_delay_ms(1);
-					PORTA &= ~(1 << PA0);
-					PORTA |= (1 << PA1);
-					_delay_ms(5);
-					if (PIND & (1 << PD2)){
-						break;
-					}
-					}
-					break;
-				}
-			}
-
-		}
+        switch (etat)
+        {
+        case 0: //rouge    
+            PORTA |= (1 << PA0);
+            PORTA &= ~(1 << PA1);
+            if (bouton_presse()){
+                etat++;
+            }
+            break;
+        case 1: //vert
+            PORTA |= (1 << PA1);
+            PORTA &= ~(1 << PA0);
+            if (bouton_presse()){
+                etat++;
+            }
+            break;
+        default: //ambre
+            PORTA &= ~(1 << PA0);
+            PORTA |= (1 << PA1);
+            _delay_ms(5);
+            PORTA |= (1 << PA0);
+            PORTA &= ~(1 << PA1);
+            _delay_ms(1);
+            if (bouton_presse()){
+                etat = 0;
+            }
+            break;
+        }
+    }
+}
+bool bouton_presse(){
+	if (PIND & (1 << PD2)){
+		_delay_ms(delay);
+        if (PIND & (1 << PD2)){
+            return true;
+        }
 	}
+	else {
+		return false;
+	}
+}
+void allumerLedRouge(){
 
 }
---
+++ TP2_probleme1.cpp
@@ -0,0 +1,87 @@
+/*Edouard Allaire
+*Abdeldjallil Moncef MESSAI
+*machines a etat pour allumer la led en pesant trois fois le bouton
+*
+* ========== I/O IDENTIFICATION (CONNEXIONS SUR LE ROBOT) =============
+* 
+*
+* ┌───────────────────────────────────────────────────────────────────┐
+* │                    ATmega324PA - Pinout                           │
+* ├───────────────────────────────────────────────────────────────────┤
+* │                                                                   │
+* │   PORTA            PORTB           PORTC          PORTD           │
+* │  ┌───────┐        ┌───────┐        ┌──────┐       ┌──────┐        │   
+* │  │ PA0 ●─│→ledR   │PB0 ○  │        │PC0 ○ │       │PD0 ○ │        │
+* │  │ PA1 ●─│→ledG   │PB1 ○  │        │PC1 ○ │       │PD1 ○ │        │
+* │  │ PA2 ○ │        │PB2 ○  │        │PC2 ○ │       │PD2 ●─│→B-INT0 │
+* │  │ PA3 ○ │        │PB3 ○  │        │PC3 ○ │       │PD3 ○ │        │
+* │  │ PA4 ○ │        │PB4 ○  │        │PC4 ○ │       │PD4 ○ │        │
+* │  │ PA5 ○ │        │PB5 ○  │        │PC5 ○ │       │PD5 ○ │        │
+* │  │ PA6 ○ │        │PB6 ○  │        │PC6 ○ │       │PD6 ○ │        │
+* │  │ PA7 ○ │        │PB7 ○  │        │PC7 ○ │       │PD7 ○ │        │
+* │  └───────┘        └───────┘        └──────┘       └──────┘        │
+* │                                                                   │
+* │  ● = Utilisé          ○ = Non utilisé                             │
+* └───────────────────────────────────────────────────────────────────┘
+*
+*bouton: input: PD2
+*led: output: PA0 et PA1
++-----------------+-----+-----------------+-----+-----+
+| etas            | PD2 | prochain etat   | PA0 | PA1 |
++-----------------+-----+-----------------+-----+-----+
+| INIT            | 0   | INT             | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| INIT            | 1   | PREMIER_APPUI   | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| PREMIER_APPUI   | 0   | PREMIER_RELACHE | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| PREMIER_APPUI   | 1   | PREMIER_APPUI   | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| PREMIER_RELACHE | 0   | PREMIER_RELACHE | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| PREMIER_RELACHE | 1   | SECOND_APPUI    | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| SECOND_APPUI    | 0   | SECOND_RELACHE  | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| SECOND_APPUI    | 1   | SECOND_APPUI    | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| SECOND_RELACHE  | 0   | SECOND_RELACHE  | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| SECOND_RELACHE  | 1   | DERNIER_APPUI   | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| DERNIER_APPUI   | 0   | DERNIER_RELACHE | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| DERNIER_APPUI   | 1   | DERNIER_APPUI   | 0   | 0   |
++-----------------+-----+-----------------+-----+-----+
+| DERNIER_RELACHE | 0   | INIT            | 0   | 1   |
++-----------------+-----+-----------------+-----+-----+
+| DERNIER_RELACHE | 1   | INIT            | 0   | 1   |
++-----------------+-----+-----------------+-----+-----+
+*/
+//boutton: entre DPD2
+//led: sortie PD1 et PD2
+#include<avr/io.h>
+#include <util/delay.h>
+const double delay = 200;
+void _delay_ms(double ms);
+bool bouton_presse();
+enum Etat (INIT,PREMIER_APPUI,PREMIER_RELACHE,SECOND_APPUI,SECOND_RELACHE,DERNIER_APPUI,DERNIER_RELACHE);
+Etat etat;
+int main(){
+    DDRA |= (1 << PA0) | (1 << PA1);
+    DDRD &= ~(1 << PD2);
+    while(true){
+        switch(etat)
+    }
+}
+bool bouton_presse(){
+	if (PIND & (1 << PD2)){
+		_delay_ms(delay);
+        if (PIND & (1 << PD2)){
+            return true;
+        }
+	}
+	else {
+		return false;
+	}
+}
