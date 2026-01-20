9334453fac60e6e8aa225da767766706beaffdda
Edouard <eefj.allaire@gmail.com>
2026-01-20 14:22:10 -0500

    TP1 EA: bouton presque parfait

--- TP1.cpp
+++ TP1.cpp
@@ -3,17 +3,15 @@
 #include <util/delay.h>
 const double ms = 1600;
 void _delay_ms(double ms);
-void ambre(int compteur);
 int main() {
 	__builtin_avr_delay_cycles(ms);
 	DDRA |= (1 << PA0) | (1 << PA1);
 	DDRD &= ~(1 << PD2);
-	PORTD |= (1 << PD2);
-	int etat = 0;
+	uint8_t etat = 0;
 	while (true) {
-		if (!(PIND & 1 << PD2)){
-			_delay_ms(50);
-			if (!(PIND & 1 << PD2)){
+		if (PIND & (1 << PD2)){
+			_delay_ms(100);
+			if (PIND & (1 << PD2)){
 				switch (etat)
 				{
 				case 0: //rouge
@@ -27,16 +25,19 @@ int main() {
 					etat++;
 					break;
 				default: //ambre
-					_delay_ms(50);
-					while (PIND & 1 << PD2) {
+					_delay_ms(100);
+					etat = 0;
+					while (true) {
 					PORTA |= (1 << PA0);
 					PORTA &= ~(1 << PA1);
 					_delay_ms(1);
 					PORTA &= ~(1 << PA0);
 					PORTA |= (1 << PA1);
-					_delay_ms(1);
+					_delay_ms(5);
+					if (PIND & (1 << PD2)){
+						break;
 					}
-					etat = 0;
+					}
 					break;
 				}
 			}
